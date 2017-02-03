1c183e6c5e0fecc872ce125d5d5b59852ba56fa7
malmax <malmax.spb@gmail.com>
2017-02-03 18:35:55 +0300

    init hw5

---
+++ lesson05/examples/3_os_.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Малахов Максим."""
+import os
+print('os.name = ', os.name)
+print('os.environ = ', os.environ)
+print('os.getcwd() = ', os.getcwd())
+
+files = os.listdir(path=os.getcwd())
+print(files)
+
+print("current dir = ", os.path.dirname(__file__))
+
+# Создаем новую директорию
+dir_path = os.path.join(os.getcwd(), 'NewDir')
+print(dir_path)
+try:
+    os.mkdir(dir_path)
+except FileExistsError:
+    print('Такая директория уже существует')
